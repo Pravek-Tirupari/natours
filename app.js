64750e2f1b78dfe7539bd7f8b65fc37ea50f58b7
Pravek Tirupari <pravektirupari@gmail.com>
2021-12-15 01:18:15 +0530

    Lecture-52 --> Handling POST Requests

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const fs = require("fs");
 const app = express();
+app.use(express.json());
 //READ TOURS DATA FROM JSON FILE
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, {
@@ -24,6 +25,21 @@ app.get("/api/v1/tours", (req, res) => {
     data: {
       tours,
     },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    });
   });
 });
 
