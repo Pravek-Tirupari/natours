93369def1aaee30ac402a3d56ecfc7e0ee97d83a
Pravek Tirupari <pravektirupari@gmail.com>
2021-12-16 01:07:09 +0530

    Create and mount multiple routers

--- app.js
+++ app.js
@@ -137,11 +137,17 @@ const deleteUser = (req, res) => {
 };
 
 //ROUTES
-app.route("/api/v1/tours").get(getAllTours).post(createTour);
-app.route("/api/v1/tours/:id").get(getTour).patch(updateTour).delete(deleteTour);
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app.route("/api/v1/users").get(getAllUsers).post(createUser);
-app.route("api/v1/users/:id").get(getUser).patch(updateUser).delete(deleteUser);
+tourRouter.route("/").get(getAllTours).post(createTour);
+tourRouter.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
+userRouter.route("/api/v1/users").get(getAllUsers).post(createUser);
+userRouter.route("api/v1/users/:id").get(getUser).patch(updateUser).delete(deleteUser);
+
+//ROUTERS
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
 const port = 3000;
 app.listen(port, () => {
