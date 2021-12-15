6bf0b7f30fb0a15923408f8a93813fdc17afc9fb
Pravek Tirupari <pravektirupari@gmail.com>
2021-12-15 11:32:23 +0530

    Lecture-54 --> Responding to URL Parameters

--- app.js
+++ app.js
@@ -28,6 +28,24 @@ app.get("/api/v1/tours", (req, res) => {
   });
 });
 
+app.get("/api/v1/tours/:id", (req, res) => {
+  console.log(req.params);
+  const id = +req.params.id;
+  if (id >= tours.length)
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid id",
+    });
+  const tour = tours.find((el) => el.id === id);
+  console.log(tour);
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post("/api/v1/tours", (req, res) => {
   console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
