5ff0b618a736cf0d8dd8f5ce1c576a30f79794a1
Pravek Tirupari <pravektirupari@gmail.com>
2021-12-29 02:19:03 +0530

    Connect to DB and setup simple Tour model

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
+const mongoose = require("mongoose");
 const app = require("./app");
+
+//CONNECT TO DB
+const DB = process.env.DB_STRING.replace("<password>", process.env.DB_PASSWORD);
+mongoose.connect(DB).then(() => console.log("Connection to DB success"));
+
+//TOUR SCHEMA
+const tourSchema = new mongoose.Schema({
+  name: { type: String, required: [true, "A tour should have a name"], unique: true },
+  rating: { type: Number, default: 4.5 },
+  price: {
+    type: Number,
+    required: [true, "A tour should have a price"],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
