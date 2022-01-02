507ed30cd93951ea4e160048dd972847a462fa68
Pravek Tirupari <pravektirupari@gmail.com>
2022-01-02 23:25:28 +0530

    Using the .create() method to store documents to DB

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,7 +21,32 @@ const getTour = (req, res) => {
   });
 };
 
-const createTour = (req, res) => {};
+const createTour = async (req, res) => {
+  // const tour = new Tour({
+  //   name: "Test Tour by save method",
+  //   price: 300,
+  //   rating: 3.3,
+  // });
+
+  //Method 1 of storing documents to the database collection
+  // tour.save();
+
+  //Method 2: Using .create() method on the Tour model directly
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
 
 const updateTour = (req, res) => {
   const id = +req.params.id;
